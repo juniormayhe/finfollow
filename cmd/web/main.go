7f8db2bc594970972228ef0ef5acdae530f5da96
Junior Mayhe <juniormayhe@gmail.com>
2022-03-26 19:49:34 +0000

    arguments added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// Define a new command-line flag with the name 'addr', a default value of
+	// and some short help text explaining what the flag controls. The value of
+	// flag will be stored in the addr variable at runtime.
+	// go run cmd/web/* -addr=":9999"
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any error
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+
+	// Use log.New() to create a logger for writing information messages. This
+	// three parameters: the destination to write the logs to (os.Stdout), a st
+	// prefix for message (INFO followed by a tab), and flags to indicate what
+	// additional information to include (local date and time). Note that the fl
+	// are joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way, but use stderr
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Llongfile)
+
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -27,7 +55,9 @@ func main() {
 	// two parameters: the TCP network address to listen on (in this case ":4000
 	// and the servemux we just created. If http.ListenAndServe() returns an er
 	// we use the log.Fatal() function to log the error message and exit.
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// log.Printf("Starting server on %s\n", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	// log.Fatal(err)
+	errorLog.Fatal(err)
 }
