14939c5c75cf77902e1b07d24aa02728251df55a
Junior Mayhe <juniormayhe@gmail.com>
2022-03-30 20:22:09 +0100

    formatting date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"juniormayhe.com/finfollow/pkg/models"
 )
@@ -15,6 +16,19 @@ type templateData struct {
 	CurrentYear int
 	Asset       *models.Asset
 	Assets      []*models.Asset
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable. This
+// essentially a string-keyed map which acts as a lookup between the names of o
+// custom template functions and the functions themselves.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -33,10 +47,19 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		// and assign it to the name variable.
 		name := filepath.Base(page)
 		// Parse the page template file in to a template set.
-		ts, err := template.ParseFiles(page)
+		// ts, err := template.ParseFiles(page)
+		// if err != nil {
+		// 	return nil, err
+		// }
+		// The template.FuncMap must be registered with the template set before
+		// call the ParseFiles() method. This means we have to use template.New
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
+
 		// Use the ParseGlob method to add any 'layout' templates to the
 		// template set (in our case, it's just the 'base' layout at the
 		// moment).
